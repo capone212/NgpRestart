ebaf02ec14d1791185c3f18e62d1c8de9424b634
anzor.apshev <anzor.apshev@fmail.com>
2015-07-13 16:18:37 -0700

    Small fix

--- SvcRestartNGP.bat
+++ SvcRestartNGP.bat
@@ -13,6 +13,7 @@ echo %time% [%loopcount%]---------- Restart NGP service... ----------
 net stop ngp_host_service
 echo %time% Stop is done!
 timeout /T 3
+echo %time% Starting service
 net start ngp_host_service
 echo %time% -----------------------------------------------
 REM ======================================================================
